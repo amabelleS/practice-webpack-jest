8e06e70bccec1cad897c53330c8a9d711da3f350
amabelle <amabelletr@gmail.com>
2021-08-07 21:12:12 +0300

    Integration test

--- util.test.js
+++ util.test.js
@@ -1,6 +1,12 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
 test('should output name and age', () => {
     const text = generateText('Amabelle', 42);
     expect(text).toBe('Amabelle (42 years old)');
 });
+
+test('should generate a valid text output', () => {
+  const text = checkAndGenerate('Amabelle', 42);
+  expect(text).toBe('Amabelle (42 years old)')
+});
+
